6524c31927c9753d86bce82a1c006b319aed001d
JS <sky77764@gmail.com>
2016-04-29 17:59:46 +0900

    0429_Button

--- _3/_3/test.cpp
+++ _3/_3/test.cpp
@@ -14,18 +14,23 @@
 #define PI 3.14159
 using namespace std;
 using namespace cv;
-static void help()
-{
-	printf("\n"
-		"This program demonstrated a simple method of connected components clean up of background subtraction\n"
-		"When the program starts, it begins learning the background.\n"
-		"You can toggle background learning on and off by hitting the space bar.\n"
-		"Call\n"
-		"./segment_objects [video file, else it reads camera 0]\n\n");
-}
+
+#define	CHAR_RECOGNITION_MODE	100
+#define NUM_RECOGNITION_MODE	200
+
+
 float distanceP2P(Point a, Point b){
 	return sqrt(fabs(pow((a).x - (b).x, 2) + pow(a.y - b.y, 2)));
 };
+
+bool checkPointInRect(Rect r, Point p){
+	if ((p.x >= r.x && p.x <= r.x + r.width) && (p.y >= r.y && p.y <= r.y + r.height))
+		return true;
+	else
+		return false;
+}
+
+
 int main(int argc, char** argv)
 {
 	VideoCapture cap;
@@ -42,11 +47,20 @@ int main(int argc, char** argv)
 		printf("can not read data from the video source\n");
 		return -1;
 	}
-	//namedWindow("segmented", 1);
-	//namedWindow("Original", 1);
 
+	// finger route
 	vector<cv::Point> trace;
 	int trace_cnt = 0;
+
+	// Character or Number recognition mode 
+	Rect NumRecogRect(10, 10, 90, 50);
+	Rect CharRecogRect(110, 10, 90, 50);
+	int RecogMode = NUM_RECOGNITION_MODE;
+	int RecogModeChange_cnt = 0;
+	int CAM_WIDTH = init.size().width;
+	int CAM_HEIGHT = init.size().height;
+	Point NumStringPoint(CAM_WIDTH - NumRecogRect.width + 10, 15 + NumRecogRect.height/2);
+	Point CharStringPoint(CAM_WIDTH - (CharRecogRect.width + NumRecogRect.width) - 5, 15 + CharRecogRect.height / 2);
 
 	try {
 		for (;;)
@@ -116,33 +130,7 @@ int main(int argc, char** argv)
 				center.y = M.m01 / M.m00;
 				//printf("Center of hull -> x: %.3f\t y: %.3f\n", center.x, center.y);
 			}
-			/*
-			//convex hull and points 
-			vector<cv::Vec4i> defects;
-			convexityDefects(handContour, hull, defects);
-			for (int k = 1; k < defects.size(); k++){
-				cv::Vec4i v = defects[k];
-				cv::Point ptStart = handContour[v[0]];
-				cv::Point ptEnd = handContour[v[1]];
-				cv::Point ptFar = handContour[v[2]];
-				float depth = v[3] / 256.0;
-
-
-				float l1 = distanceP2P(ptStart, ptFar);
-				float l2 = distanceP2P(ptEnd, ptFar);
-				float dot = (ptStart.x - ptFar.x)*(ptEnd.x - ptFar.x) + (ptStart.y - ptFar.y)*(ptEnd.y - ptFar.y);
-				float angle = acosf(dot / (l1*l2));
-				angle = angle * 180 / PI;
-
-				if (angle <95 && depth > 10){
-					line(original, ptStart, ptFar, cv::Scalar(0, 255, 0), 2);
-					line(original, ptEnd, ptFar, cv::Scalar(0, 255, 0), 2);
-					circle(original, ptStart, 6, cv::Scalar(k * 10, 150, 150), 2);
-					circle(original, ptEnd, 6, cv::Scalar(0, 0, 255), 2);
-					circle(original, ptFar, 6, cv::Scalar(0, 0, 255), 2);
-				}
-			}
-			*/
+			
 				
 			//making mask
 			Point2f center;
@@ -170,7 +158,7 @@ int main(int argc, char** argv)
 				
 			
 			else {
-				printf("%d\n", num_label);
+				//printf("%d\n", num_label);
 				trace_cnt++;
 				if (trace_cnt >= 7)
 					trace.clear();
@@ -183,17 +171,57 @@ int main(int argc, char** argv)
 				line(original, trace[i-1], trace[i], Scalar(255, 100, 0), 5);
 			}
 			cout << endl;
-			
 
+
+			Mat overlay(original.size(), original.type());
+			original.copyTo(overlay);
+
+			if (checkPointInRect(CharRecogRect, handContour[min_idx])){
+				RecogModeChange_cnt++;
+				if (RecogModeChange_cnt == 7) {
+					RecogMode = CHAR_RECOGNITION_MODE;
+					RecogModeChange_cnt = 0;
+					trace.clear();
+					printf("CHARACTER RECOGNITION MODE!\n");
+				}
+			}
+			else if (checkPointInRect(NumRecogRect, handContour[min_idx])){
+				RecogModeChange_cnt++;
+				if (RecogModeChange_cnt == 7) {
+					RecogMode = NUM_RECOGNITION_MODE;
+					RecogModeChange_cnt = 0;
+					trace.clear();
+					printf("NUMBER RECOGNITION MODE!\n");
+				}
+			}
+			else
+				RecogModeChange_cnt = 0;
+
+
+			if (RecogMode == CHAR_RECOGNITION_MODE) {
+				rectangle(overlay, CharRecogRect, CV_RGB(50, 205, 50), -1);
+				rectangle(overlay, NumRecogRect, CV_RGB(128, 128, 128), -1);
+			}
+			else {
+				rectangle(overlay, CharRecogRect, CV_RGB(128, 128, 128), -1);
+				rectangle(overlay, NumRecogRect, CV_RGB(50, 205, 50), -1);
+			}
+
+			addWeighted(overlay, 0.5, original, 1 - 0.5, 0, original);
 
 			flip(original, original, 1);
-			flip(binaryMat, binaryMat, 1);
+			//flip(binaryMat, binaryMat, 1);
 			flip(img_parted, img_parted, 1);
+
+			putText(original, "NUM", NumStringPoint, 2, 0.7, Scalar::all(255));
+			putText(original, "CHAR", CharStringPoint, 2, 0.7, Scalar::all(255));
 			
+			
+
 			imshow("Original", original);
 			//imshow("Segmented", temp);
 			//imshow("binary", binaryMat);
-			imshow("Parted", img_parted);
+			//imshow("Parted", img_parted);
 			
 			
 			int keycode = waitKey(30);
@@ -209,3 +237,32 @@ int main(int argc, char** argv)
 	
 	return 0;
 }
+
+
+/*
+//convex hull and points
+vector<cv::Vec4i> defects;
+convexityDefects(handContour, hull, defects);
+for (int k = 1; k < defects.size(); k++){
+cv::Vec4i v = defects[k];
+cv::Point ptStart = handContour[v[0]];
+cv::Point ptEnd = handContour[v[1]];
+cv::Point ptFar = handContour[v[2]];
+float depth = v[3] / 256.0;
+
+
+float l1 = distanceP2P(ptStart, ptFar);
+float l2 = distanceP2P(ptEnd, ptFar);
+float dot = (ptStart.x - ptFar.x)*(ptEnd.x - ptFar.x) + (ptStart.y - ptFar.y)*(ptEnd.y - ptFar.y);
+float angle = acosf(dot / (l1*l2));
+angle = angle * 180 / PI;
+
+if (angle <95 && depth > 10){
+line(original, ptStart, ptFar, cv::Scalar(0, 255, 0), 2);
+line(original, ptEnd, ptFar, cv::Scalar(0, 255, 0), 2);
+circle(original, ptStart, 6, cv::Scalar(k * 10, 150, 150), 2);
+circle(original, ptEnd, 6, cv::Scalar(0, 0, 255), 2);
+circle(original, ptFar, 6, cv::Scalar(0, 0, 255), 2);
+}
+}
+*/
